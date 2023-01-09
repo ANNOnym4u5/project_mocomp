b8fad1c788714bee77d31de60b367ef68d5e0183
mariayerossi <maria_y20@mhs.istts.ac.id>
2023-01-09 13:31:54 +0700

    login

--- app/src/main/java/com/example/doup_app/MainActivity.kt
+++ app/src/main/java/com/example/doup_app/MainActivity.kt
@@ -4,10 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
+    lateinit var etuser: EditText
+    lateinit var etpass: EditText
     lateinit var btnLogin : Button
     lateinit var btnToRegis : Button
     lateinit var l: ActivityResultLauncher<Intent>
@@ -15,6 +19,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        etuser = findViewById(R.id.edUser)
+        etpass = findViewById(R.id.edPass)
         btnLogin = findViewById(R.id.btnLogin)
         btnToRegis = findViewById(R.id.btnToRegis)
 
@@ -31,6 +37,38 @@ class MainActivity : AppCompatActivity() {
             val i= Intent(this,register_page::class.java)
             i.putExtra("user",users)
             l.launch(i)
+        }
+        btnLogin.setOnClickListener {
+            val user=etuser.text.toString()
+            val pass=etpass.text.toString()
+            if (user.isNotEmpty()&&pass.isNotEmpty()){
+                var login=false
+                for (u in users){
+                    if (u.user==user&&u.pass==pass){
+                        login=true
+//                        if (u is Dokter){
+//                            val i=Intent(this,DokterActivity::class.java)
+//                            i.putParcelableArrayListExtra("user",users)
+//                            i.putExtra("idx",users.indexOf(u).toString())
+//                            l.launch(i)
+//                        }else{
+//                            val i=Intent(this,PasienActivity::class.java)
+//                            i.putParcelableArrayListExtra("user",users)
+//                            i.putExtra("idx",users.indexOf(u).toString())
+//                            l.launch(i)
+//                        }
+                        val i=Intent(this,PasienActivity::class.java)
+                        i.putParcelableArrayListExtra("user",users)
+                        i.putExtra("idx",users.indexOf(u).toString())
+                        l.launch(i)
+                    }
+                }
+                if (!login){
+                    Toast.makeText(this,"Username dan Password tidak sesuai!", Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                Toast.makeText(this,"Inputan tidak boleh kosong!", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
---
+++ app/src/main/java/com/example/doup_app/PasienActivity.kt
@@ -0,0 +1,11 @@
+package com.example.doup_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PasienActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pasien)
+    }
+}
